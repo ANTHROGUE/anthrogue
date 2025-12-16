3ecb2dd64d466f427426518521bd81ad00a26331
Buckstrom <58726188+Buckstrom@users.noreply.github.com>
2025-12-16 10:44:56 -0800

    LevelManager: Initialized

---
+++ ar/globals/zone_service.gd
@@ -0,0 +1,4 @@
+extends Node
+
+var manager: LevelManager
+var current_zone: Zone
---
+++ game/level/level_manager.gd
@@ -0,0 +1,5 @@
+extends Node
+class_name LevelManager
+
+var zones: Array[Array]
+@export var zone_map_range: Vector2i = Vector2i(8, 4)
