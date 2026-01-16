4a9f86307dd523169630b87beb91fb9601de14b8
NitroSoggy <ckblaze2012@gmail.com>
2026-01-16 11:31:30 -0800

    Test Block Interval
    Added the interval for test block, allowing it to be used on self in combat.

--- ar/registry/battle/weapons/crowbar/crowbar_block.gd
+++ ar/registry/battle/weapons/crowbar/crowbar_block.gd
@@ -1,10 +1,9 @@
 extends ActionScript
 
 func action() -> void:
-	#ival_path = "uid://c80tj678kqkkk"
+	ival_path = "uid://bpu5e70h8m72l"
 	super()
-	await get_tree().create_timer(1.0).timeout
-	end_action()
+
 
 func end_action() -> void:
 	super()
